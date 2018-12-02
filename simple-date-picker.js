9c4499c96242cfc4b1814d51529de9161a565541
Rodrigo Salado Anaya <rodrigo.salado.anaya@gmail.com>
2018-12-01 18:21:30 -0600

    Add base element

---
+++ simple-date-picker.js
@@ -0,0 +1,19 @@
+import {LitElement, html} from '@polymer/lit-element/lit-element.js';
+
+/**
+ * `simple-date-picker`
+ * Date picker with LitElement
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class SimpleDatePicker extends LitElement {
+  render() {
+    return html`
+      1er test
+    `;
+  }
+}
+
+window.customElements.define('simple-date-picker', SimpleDatePicker);
