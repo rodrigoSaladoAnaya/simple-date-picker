022963d2ec65eae07e08da9d4122eb51fbe37151
Rodrigo Salado Anaya <rodrigo.salado.anaya@gmail.com>
2018-12-05 00:42:51 -0600

    add forward and back button

--- simple-date-picker.js
+++ simple-date-picker.js
@@ -27,11 +27,35 @@ class SimpleDatePicker extends LitElement {
       }
       #container {
         margin-top: 0.6em;
-        background-color: var(--sdp-container-background-color, #fbfbfb);
+        background-color: var(--sdp-container-background-color, #fff);
+        border: 0.05em solid var(--sdp-container-border, #dfdfdf);
+      }
+      #header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+      }
+      svg {
+        width: 2.5em;
+        height: 2.5em;
+        cursor: pointer;
+        margin: 0.5em;
+      }
+      svg:hover {
+        background-color: var(--sdp-goto-background-color, #f6f6f6);
       }
       </style>
       <input typ="text" maxlength="10" placeholder="YYYY-MM-DD"></input>
       <div id="container">
+        <div id="header">
+          <svg viewBox="0 0 48 48">
+            <polygon points="15.808,24 26.301,13.507 29.973,17.179 23.161,24 29.977,30.816 26.302,34.492"/>
+          </svg>
+          <div>Dic, 2018</div>
+          <svg viewBox="0 0 48 48">
+            <polygon points="32.227,24 21.734,13.507 18.062,17.179 24.874,24 18.058,30.816 21.733,34.492"/>
+          </svg>
+        </div>
         <sdp-weeks now="2018-12-04"></sdp-weeks>
       </div>
     `;
