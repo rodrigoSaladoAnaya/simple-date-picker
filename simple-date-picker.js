b17d4e2ab2c23b616ab458eb287001936528874d
Rodrigo Salado Anaya <rodrigo.salado.anaya@gmail.com>
2018-12-04 19:30:26 -0600

    skeleton of day element

--- simple-date-picker.js
+++ simple-date-picker.js
@@ -1,5 +1,5 @@
 import {LitElement, html} from '@polymer/lit-element/lit-element.js';
-
+import './lib/sdp-day.js';
 /**
  * `simple-date-picker`
  * Date picker with LitElement
@@ -7,11 +7,31 @@ import {LitElement, html} from '@polymer/lit-element/lit-element.js';
  * @customElement
  * @polymer
  * @demo demo/index.html
+ * blue: #3FA6FD
  */
 class SimpleDatePicker extends LitElement {
   render() {
     return html`
-      1er test
+      <style>
+      :host {
+        display: block;
+        width: 15em;
+      }
+      :host > * {
+        box-sizing: border-box;
+      }
+      input {
+        width: 100%;
+      }
+      #container {
+        width: 100%;
+        border: 1px solid #bebebe;
+      }
+      </style>
+      <input typ="text" maxlength="8"></input>
+      <div id="container">
+        <sdp-day>1</sdp-day>
+      </div>
     `;
   }
 }
