de09260f746f2ce439acb70073c74e3371e8954c
Rodrigo Salado Anaya <rodrigo.salado.anaya@gmail.com>
2018-12-09 11:48:09 -0600

    Add day labels

--- simple-date-picker.js
+++ simple-date-picker.js
@@ -1,6 +1,6 @@
 import {LitElement, html} from '@polymer/lit-element/lit-element.js';
 import {styles} from './lib/sdp-style.js'
-import {backButton, forwardButton, shortDateLable} from './lib/sdp-header.js';
+import {backButton, forwardButton, shortDateLable, dayLabels} from './lib/sdp-header.js';
 import {go_back, go_forward} from './lib/sdp-utils.js';
 
 class SimpleDatePicker extends LitElement {
@@ -19,6 +19,9 @@ class SimpleDatePicker extends LitElement {
         ${backButton(go_back.bind(this))}
         ${shortDateLable(this.date)}
         ${forwardButton(go_forward.bind(this))}
+      </div>
+      <div class="day-label-s">
+        ${dayLabels()}
       </div>
     `;
   }
